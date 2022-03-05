cf51511982789fbbe416db154a19e2e79dac065d
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-05 13:53:42 -0500

    add CSS and quetsions to js

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+let questions = [
+  { question: "Commonly used data types do NOT include:",
+    answers: {
+      a: "Strings",
+      b: "Booleans",
+      c: "Alerts",
+      d: "Numbers",
+    },
+    correctAnswer: "b",
+  },
+  {
+    question: "The condition in an if / else statement is enclosed with _______:",
+    answers: {
+      a: "Quotes",
+      b: "Curly Brackets",
+      c: "Parenthesis",
+      d: "Square Brackets",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ______.",
+    answers: {
+      a: "Numbers and Strings",
+      b: "Other Arrays",
+      c: "Booleans",
+      d: "All of the above",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    answers: {
+      a: "Commas",
+      b: "Curly Brackets",
+      c: "Quotes",
+      d: "Parenthesis",
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: {
+      a: "JavaScript",
+      b: "Terminal/bash",
+      c: "For Loops",
+      d: "console.log",
+    },
+    correctAnswer: "c"
+  },
+];
